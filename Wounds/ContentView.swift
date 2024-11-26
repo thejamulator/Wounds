66dd238cd701873c5759a61180d37783552dd527
Eric Ford <thejamulator@gmail.com>
2024-11-25 16:34:45 -0800

    Initial Commit

---
+++ Wounds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2024 and Confidential to Eric Ford Consulting All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wounds/WoundsApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2024 and Confidential to Eric Ford Consulting All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
